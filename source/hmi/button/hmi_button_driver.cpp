c90fce73fc4141a921c14f28fcaeb2277f2f9a71
brandonbraun653 <brandonbraun653@gmail.com>
2020-11-08 22:57:41 -0600

    Implemented the button driver Closes #11

--- source/hmi/button/hmi_button_driver.cpp
+++ source/hmi/button/hmi_button_driver.cpp
@@ -11,7 +11,287 @@
 /* Aurora Includes */
 #include <Aurora/hmi>
 
+/* Chimera Includes */
+#include <Chimera/exti>
+#include <Chimera/function>
+#include <Chimera/gpio>
+#include <Chimera/scheduler>
+
 namespace Aurora::HMI::Button
 {
+  /*-------------------------------------------------------------------------------
+  EdgeTrigger Implementation
+  -------------------------------------------------------------------------------*/
+  EdgeTrigger::EdgeTrigger() : mCallback( EdgeCallback() ), mConfig( {} ), mNumEvents( 0 )
+  {
+  }
+
+
+  EdgeTrigger::~EdgeTrigger()
+  {
+  }
+
+
+  /*-------------------------------------------------
+  Public Methods
+  -------------------------------------------------*/
+  bool EdgeTrigger::initialize( const EdgeConfig &cfg )
+  {
+    /*-------------------------------------------------
+    Input protection
+    -------------------------------------------------*/
+    /* clang-format off */
+    if ( !cfg.gpioConfig.validity ||
+         !cfg.stableSamples ||
+         !cfg.sampleRate ||
+         !( cfg.sampleRate < cfg.debounceTime ) ||
+         !( cfg.activeEdge < ActiveEdge::NUM_OPTIONS ) )
+    { /* clang-format on */
+      return false;
+    }
+    else
+    {
+      mLock.lock();
+      mConfig = cfg;
+    }
+
+    /*-------------------------------------------------
+    Reset to a known state
+    -------------------------------------------------*/
+    this->reset();
+
+    /*-------------------------------------------------
+    Initialize the scheduler timer
+    -------------------------------------------------*/
+    Chimera::Scheduler::LoRes::open();
+
+    /*-------------------------------------------------
+    Convert the HMI edge type to the EXTI type
+    -------------------------------------------------*/
+    Chimera::EXTI::EdgeTrigger edgeTrigger;
+
+    switch ( mConfig.activeEdge )
+    {
+      case ActiveEdge::BOTH_EDGES:
+        edgeTrigger = Chimera::EXTI::EdgeTrigger::BOTH_EDGE;
+        break;
+
+      case ActiveEdge::RISING_EDGE:
+        edgeTrigger = Chimera::EXTI::EdgeTrigger::RISING_EDGE;
+        break;
+
+      case ActiveEdge::FALLING_EDGE:
+        edgeTrigger = Chimera::EXTI::EdgeTrigger::FALLING_EDGE;
+        break;
+
+      default:
+        edgeTrigger = Chimera::EXTI::EdgeTrigger::UNKNOWN;
+        break;
+    };
+
+    /*-------------------------------------------------
+    Configure the GPIO
+    -------------------------------------------------*/
+    auto result = Chimera::Status::OK;
+    auto cb     = Chimera::Function::vGeneric::create<EdgeTrigger, &EdgeTrigger::gpioEdgeTriggerCallback>( *this );
+    auto driver = Chimera::GPIO::getDriver( mConfig.gpioConfig.port, mConfig.gpioConfig.pin );
+
+    result |= driver->init( mConfig.gpioConfig );
+    result |= driver->attachInterrupt( cb, edgeTrigger );
+
+    mLock.unlock();
+    return ( result == Chimera::Status::OK );
+  }
+
+
+  void EdgeTrigger::reset()
+  {
+    mLock.lock();
+
+    /*-------------------------------------------------
+    Reset the trackers
+    -------------------------------------------------*/
+    mNumEvents = 0;
+    mDebounced = 0;
+
+    /*-------------------------------------------------
+    Configure GPIO to have minimal system impact
+    -------------------------------------------------*/
+    auto driver = Chimera::GPIO::getDriver( mConfig.gpioConfig.port, mConfig.gpioConfig.pin );
+    driver->setMode( Chimera::GPIO::Drive::HIZ, Chimera::GPIO::Pull::NO_PULL );
+    driver->detachInterrupt();
+
+    /*-------------------------------------------------
+    Disable the GPIO's EXTI hooks
+    -------------------------------------------------*/
+    Chimera::EXTI::disable( driver->getInterruptLine() );
+
+    mLock.unlock();
+  }
+
+
+  void EdgeTrigger::enable()
+  {
+    auto driver = Chimera::GPIO::getDriver( mConfig.gpioConfig.port, mConfig.gpioConfig.pin );
+    Chimera::EXTI::enable( driver->getInterruptLine() );
+  }
+
+
+  void EdgeTrigger::disable()
+  {
+    auto driver = Chimera::GPIO::getDriver( mConfig.gpioConfig.port, mConfig.gpioConfig.pin );
+    Chimera::EXTI::disable( driver->getInterruptLine() );
+  }
+
+
+  void EdgeTrigger::onActiveEdge( EdgeCallback callback )
+  {
+    mCallback = callback;
+  }
+
+
+  size_t EdgeTrigger::numEdgeEvents()
+  {
+    mLock.lock();
+    auto tmp = mNumEvents;
+    mLock.unlock();
+
+    return tmp;
+  }
+
+
+  ActiveEdge EdgeTrigger::getActiveEdge()
+  {
+    mLock.lock();
+    auto tmp = mConfig.activeEdge;
+    mLock.unlock();
+
+    return tmp;
+  }
+
+
+  /*-------------------------------------------------
+  Private Methods
+  -------------------------------------------------*/
+  void EdgeTrigger::gpioEdgeTriggerCallback( void *arg )
+  {
+    /*-------------------------------------------------
+    Disable the interrupt as quickly as possible
+    -------------------------------------------------*/
+    this->disable();
+
+    /*-------------------------------------------------
+    Figure out the number of samples the polling
+    function should be active for. Div zero protected
+    by the initialization sequence.
+    -------------------------------------------------*/
+    size_t numSamples = mConfig.debounceTime / mConfig.sampleRate;
+
+    /*-------------------------------------------------
+    Register a periodic function with the scheduler to
+    poll the GPIO state for a bit.
+    -------------------------------------------------*/
+    auto cb = Chimera::Function::Opaque::create<EdgeTrigger, &EdgeTrigger::gpioEdgeSamplerCallback>( *this );
+
+    Chimera::Scheduler::LoRes::periodic( cb, mConfig.sampleRate, numSamples );
+  }
+
+
+  void EdgeTrigger::gpioEdgeSamplerCallback()
+  {
+    /*-------------------------------------------------
+    Read the current state of the GPIO pin
+    -------------------------------------------------*/
+    Chimera::GPIO::State currentState;
+    auto driver = Chimera::GPIO::getDriver( mConfig.gpioConfig.port, mConfig.gpioConfig.pin );
+    driver->getState( currentState );
+
+    /*-------------------------------------------------
+    Shift the filter because a single sample has passed
+    -------------------------------------------------*/
+    mDebounced = mDebounced << 1u;
+
+    /*-------------------------------------------------
+    Assuming the state is valid, fill in the spot just
+    created by the shift. Forcefully set the zero as
+    shift hardware technically could be circular and we
+    might run out of bits.
+    -------------------------------------------------*/
+    if ( mConfig.activeEdge == ActiveEdge::BOTH_EDGES )
+    {
+      mDebounced |= 1u;
+    }
+    else
+    {
+      switch ( mConfig.activeEdge )
+      {
+        case ActiveEdge::RISING_EDGE:
+          if ( currentState == Chimera::GPIO::State::HIGH )
+          {
+            mDebounced |= 1u;
+          }
+          else
+          {
+            mDebounced &= ~1u;
+          }
+          break;
+
+        case ActiveEdge::FALLING_EDGE:
+          if ( currentState == Chimera::GPIO::State::LOW )
+          {
+            mDebounced |= 1u;
+          }
+          else
+          {
+            mDebounced &= ~1u;
+          }
+          break;
+
+        default:
+          mDebounced &= ~1u;
+          break;
+      };
+    }
+
+    /*-------------------------------------------------
+    Build the debounced mask
+    -------------------------------------------------*/
+    size_t debounceMask = 0;
+    for ( size_t x = 0; x < mConfig.stableSamples; x++ )
+    {
+      debounceMask |= ( 1u << x );
+    }
+
+    /*-------------------------------------------------
+    Have enough samples been stable to consider this a
+    sufficiently debounced button?
+    -------------------------------------------------*/
+    if ( ( mDebounced & debounceMask ) == debounceMask )
+    {
+      /*-------------------------------------------------
+      Update trackers
+      -------------------------------------------------*/
+      mLock.lock();
+      mNumEvents++;
+      mLock.unlock();
+
+      mDebounced = 0;
+      Chimera::Scheduler::LoRes::cancel_this();
+
+      /*-------------------------------------------------
+      Re-enable the trigger to listen for more presses
+      -------------------------------------------------*/
+      this->enable();
+
+      /*-------------------------------------------------
+      Invoke user callback if exists
+      -------------------------------------------------*/
+      if ( mCallback )
+      {
+        mCallback( mConfig.activeEdge );
+      }
+    }
+    // else button not sufficiently debounced yet
+  }
 
 }  // namespace Aurora::HMI::Button
--- source/hmi/button/hmi_button_driver.hpp
+++ source/hmi/button/hmi_button_driver.hpp
@@ -3,7 +3,10 @@
  *    hmi_button_driver.hpp
  *
  *  Description:
- *    Human machine interface driver for a GPIO based button
+ *    Human machine interface driver for a GPIO based button. The purpose of this
+ *    driver is to provide clean event signals for when a button has been pressed
+ *    or released. Any higher level functionalities like hold behavior or system
+ *    level notification is left to another library.
  *
  *  2020 | Brandon Braun | brandonbraun653@gmail.com
  *******************************************************************************/
@@ -15,66 +18,120 @@
 /* Chimera Includes */
 #include <Chimera/function>
 #include <Chimera/gpio>
+#include <Chimera/thread>
 
 namespace Aurora::HMI::Button
 {
   /*-------------------------------------------------------------------------------
   Enumerations
   -------------------------------------------------------------------------------*/
-  enum class ActiveState : uint8_t
+  enum class ActiveEdge : uint8_t
   {
-    ACTIVE_LOW,   /**< Signal active on stable logic low */
     RISING_EDGE,  /**< Signal active on rising edge triggers */
-    ACTIVE_HIGH,  /**< Signal active on stable logic high */
     FALLING_EDGE, /**< Signal active on falling edge triggers */
+    BOTH_EDGES,   /**< Signal active on both edges */
 
     NUM_OPTIONS,
     UNKNOWN
   };
 
   /*-------------------------------------------------------------------------------
+  Aliases
+  -------------------------------------------------------------------------------*/
+  using EdgeCallback = etl::delegate<void( ActiveEdge )>;
+
+  /*-------------------------------------------------------------------------------
   Structures
   -------------------------------------------------------------------------------*/
-  struct Config
+  struct EdgeConfig
   {
-    Chimera::GPIO::PinInit pinConfig;
-    ActiveState activeState;
-    size_t debounceTime;
+    Chimera::GPIO::PinInit gpioConfig; /**< GPIO hardware configuration */
+    ActiveEdge activeEdge;             /**< Which edge to trigger on */
+    size_t debounceTime;               /**< Max sample time before GPIO is considered debounced (mS) */
+    size_t sampleRate;                 /**< Period at which to sample GPIO logic state (mS) */
+    size_t stableSamples;              /**< Number of samples at which to consider GPIO state stable */
   };
 
   /*-------------------------------------------------------------------------------
   Classes
   -------------------------------------------------------------------------------*/
-  class Driver
+  class EdgeTrigger
   {
   public:
-    Driver();
-    ~Driver();
+    EdgeTrigger();
+    ~EdgeTrigger();
 
     /**
+     *  Initializes the system and hardware for processing GPIO edge events.
      *
+     *  @param[in]  cfg     The configuration to be applied
+     *  @return bool
      */
-    bool initialize( const Config &cfg );
+    bool initialize( const EdgeConfig &cfg );
 
     /**
+     *  Resets the driver to the same state as if 'initialize' was just called.
+     *  This will keep any existing hardware settings, but disables the GPIO
+     *  listener processing and removes any registered callbacks.
      *
+     *  @return void
+     */
+    void reset();
+
+    /**
+     *  Enables listening to the GPIO pin for the active edge transition. Must
+     *  be called after initializing the driver in order for processing to work.
+     *
+     *  @return void
      */
     void enable();
 
     /**
+     *  Disables the GPIO listeners that handle edge event processing. This
+     *  does not destroy any configurations. Re-enable listening by calling
+     *  'enable()'.
      *
+     *  @return void
      */
     void disable();
 
     /**
+     *  Registers some function to perform upon a successfully debounced
+     *  edge event.
      *
+     *  @note The call to this function will occur in thread mode, so use
+     *  whatever systems resources are needed. There are no ISR constraints.
+     *
+     *  @param[in]  callback        The function to be executed
+     *  @return void
      */
-    void onTrigger( Chimera::Function::Opaque &callback );
+    void onActiveEdge( EdgeCallback callback );
+
+    /**
+     *  How many edge events have occurred since the last time this function
+     *  was called. Upon calling, will clear the recorded number of events.
+     *
+     *  @return size_t
+     */
+    size_t numEdgeEvents();
+
+    /**
+     *  Gets the currently configured active edge state
+     *  @return ActiveEdge
+     */
+    ActiveEdge getActiveEdge();
+
 
   private:
-    Chimera::Function::Opaque mTrigCallback;
-    Chimera::GPIO::Driver_sPtr mPin;
+    EdgeCallback mCallback;
+    EdgeConfig mConfig;
+    size_t mNumEvents;
+    size_t mDebounced;
+    Chimera::Threading::RecursiveMutex mLock;
+
+    void gpioEdgeTriggerCallback( void *arg );
+    void gpioEdgeSamplerCallback();
   };
 }  // namespace Aurora::HMI::Button
 
-#endif  /* !AURORA_HMI_GPIO_BUTTON_HPP */
+#endif /* !AURORA_HMI_GPIO_BUTTON_HPP */
