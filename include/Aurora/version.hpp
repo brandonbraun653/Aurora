6c9e26b25683a2baf1744b3f75062b8741a5752a
Brandon Braun <brandonbraun653@gmail.com>
2020-12-19 13:13:30 -0600

    Add version file v1.0.0

---
+++ include/Aurora/version.hpp
@@ -0,0 +1,23 @@
+/********************************************************************************
+ *  File Name:
+ *    version.hpp
+ *
+ *  Description:
+ *    Aurora version information
+ *
+ *  2020 | Brandon Braun | brandonbraun653@gmail.com
+ ********************************************************************************/
+
+#pragma once
+#ifndef AURORA_VERSION_HPP
+#define AURORA_VERSION_HPP
+
+/* STL Includes */
+#include <string_view>
+
+namespace Aurora
+{
+  static constexpr std::string_view version = "1.0.0";
+}
+
+#endif /* !AURORA_VERSION_HPP */
