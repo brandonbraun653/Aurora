9d8297ae0dd234fe9b111651846235d9eb8cbd1a
Brandon Braun <brandonbraun653@gmail.com>
2021-04-30 06:53:13 -0500

    Add bitwise operator declarations for enum classes

--- source/util/enum.hpp
+++ source/util/enum.hpp
@@ -29,5 +29,16 @@ constexpr typename std::underlying_type<E>::type EnumValue( E e ) noexcept
   return static_cast<typename std::underlying_type<E>::type>( e );
 }
 
+/**
+ * @brief Declares a bitwise operator overload for an enum class
+ *
+ * @param cls     Enum class to define operator on
+ * @param op      Bitwise operator
+ */
+#define ENUM_CLS_BITWISE_OPERATOR( cls, op )                                        \
+  inline constexpr cls operator op( cls x, cls y )                                  \
+  {                                                                                 \
+    return static_cast<cls>( static_cast<size_t>( x ) | static_cast<size_t>( y ) ); \
+  }
 
 #endif /* !AURORA_UTIL_ENUM_HPP */
