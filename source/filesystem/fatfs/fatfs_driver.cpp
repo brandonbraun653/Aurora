86a3c6b65e30a0acc150c652db3539f65240f03c
Brandon Braun <brandonbraun653@protonmail.com>
2023-09-30 17:10:58 -0700

    Start working on FatFs integration

---
+++ source/filesystem/fatfs/fatfs_driver.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+ *  File Name:
+ *    fatfs_driver.cpp
+ *
+ *  Description:
+ *    FatFS driver implementation for Aurora
+ *
+ *  2023 | Brandon Braun | brandonbraun653@protonmail.com
+ *****************************************************************************/
+
+/*-----------------------------------------------------------------------------
+Includes
+-----------------------------------------------------------------------------*/
+
+
+namespace Aurora::FileSystem::FatFs
+{
+
+}  // namespace Aurora::FileSystem::FatFs
